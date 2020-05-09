a66707b8b0690e3db54334f65c287d52d895700f
zhengbo <jesn2013@hotmail.com>
2020-05-09 09:33:47 +0800

    test pipelines build

--- github_action_test/Startup.cs
+++ github_action_test/Startup.cs
@@ -25,6 +25,7 @@ namespace github_action_test
             services.AddRazorPages();
         }
 
+        // test  submit
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
